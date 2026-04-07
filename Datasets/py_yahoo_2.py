a4d1aa7f9b2d00e3c1ae6ade9af44ee177cd6620
cvishnu007 <c.vishnutheertha@gmail.com>
2026-04-07 11:27:02 +0530

    removed redundant files

--- Datasets/py_yahoo_2.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-
-prices = yf.download("AAPL", start="2015-01-01", end="2024-12-31")
-prices["return_1d"] = prices["Close"].pct_change().shift(-1)
-
-prices.to_csv("yahoo_2.csv",index=False)
